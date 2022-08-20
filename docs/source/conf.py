ed79ec892ad2a921e0b2e2e4b895fcee2f044491
Rafael Karosuo <rafaelkarosuo@gmail.com>
2022-08-20 03:43:15 -0700

    Fix, actualizar source/conf.py con el nombre del proyecto, las versiones, el autor y la licencia correcta, en vez de usar los valores por defecto del proyecto template de read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CoDe Pretzel Framework'
+copyright = 'Apache 2.0 License'
+author = 'Rafael Karosuo'
 
 release = '0.1'
 version = '0.1.0'
